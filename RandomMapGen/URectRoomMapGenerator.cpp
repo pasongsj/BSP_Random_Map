cf55eb0bf7c4b266661129f6906e869b8afbaa15
SujeongLee <audrbekf@gmail.com>
2024-02-02 22:56:26 +0800

    리프노드 순서로 인한 오류 임시수정

--- RandomMapGen/URectRoomMapGenerator.cpp
+++ RandomMapGen/URectRoomMapGenerator.cpp
@@ -32,7 +32,7 @@ bool URectRoomMapGenerator::CreateMap()
             RootNode = std::make_shared<Node>();
             RootNode->nodeRect = { 1, 1, lx - 1, ly - 1 }; // 맵의 테두리를 제외한 크기의 루트노드
         }
-        bool can_gen = DivideNode(RootNode, room_cnt, min_room_size, _rate);
+        bool can_gen = DivideNode(RootNode, room_cnt, min_room_size, _rate, true);
 
         if (true == can_gen) // 랜덤맵 생성 성공
         {
@@ -63,7 +63,7 @@ bool URectRoomMapGenerator::CreateMap()
     int index = 1;
     for (std::shared_ptr<Node> _leaf : LeafNodeList)
     {
-        //std::cout << index++ << ':' << _leaf->nodeRect.x << ' ' << _leaf->nodeRect.y << ' ' << _leaf->nodeRect.x + _leaf->nodeRect.height << ' ' << _leaf->nodeRect.y + _leaf->nodeRect.width << '\n';
+        //std::cout << index++ << ':' << _leaf->nodeRect.x << ' ' << _leaf->nodeRect.x + _leaf->nodeRect.height << ' ' << _leaf->nodeRect.y  <<  ' ' << _leaf->nodeRect.y + _leaf->nodeRect.width << '\n';
         CreateRoom(_leaf);
     }
     auto it = LeafNodeList.begin();
@@ -233,6 +233,145 @@ bool URectRoomMapGenerator::DivideNode(std::shared_ptr<Node> tree, int n, int _s
 
 }
 
+bool URectRoomMapGenerator::DivideNode(std::shared_ptr<Node> tree, int n, int _size, float _rate, bool is_reverse)
+{
+    if (n == 1) // 더이상 방을 나눌 필요가 없을 때
+    {
+        if (_size > GetRoomSize(tree->nodeRect)) // 최소 방 크기를 만족하지 않는다면
+        {
+            return false;
+        }
+        LeafNodeList.push_back(tree);
+        return true;
+    }
+
+
+    // 방을 더 나눠야 할 때
+
+    // 현재 노드의 방 정보
+    const RectInt curRect = tree->nodeRect;
+
+    // 가로로 나눌지 세로로 나눌지
+    int maxLength = curRect.height > curRect.width ? curRect.height : curRect.width;;
+    bool is_height = curRect.height > curRect.width ? true : false;
+
+    // 5:5 비율로 나눈다고 가정
+    int split = maxLength / 2;
+
+    // 왼쪽노드의 방 개수
+    int leftnodecnt = n / 2;
+    // 오른쪽 노드의 방 개수
+    int rightnodecnt = n - leftnodecnt;
+
+    // 최대 방 크기 * 여분 비율
+    int max_room_size = static_cast<int>((_size + 1) * 2 * _rate);
+
+
+    // 5:5로 나누었을 때 Left와 Right임시값
+    RectInt LeftRect, RightRect;
+    GetChildRect(curRect, split, is_height, LeftRect, RightRect);
+
+    int leftSize = GetRoomSize(LeftRect);
+    int rightSize = GetRoomSize(RightRect);
+
+    // 한쪽이 모두 -1인경우 방을 만들 수 없기 때문에 반대쪽 노드에 n개만큼 생성한다.
+    if (leftSize == 0)
+    {
+        tree->rightNode = std::make_shared<Node>();
+        tree->rightNode->nodeRect = RightRect;
+        tree->rightNode->parNode = tree;
+        return DivideNode(tree->rightNode, n, _size, _rate);//왼쪽, 오른쪽 자식 노드들도 나눠준다.
+    }
+    else if (rightSize == 0)
+    {
+        tree->leftNode = std::make_shared<Node>();
+        tree->leftNode->nodeRect = LeftRect;
+        tree->leftNode->parNode = tree;
+        return DivideNode(tree->leftNode, n, _size, _rate);//왼쪽, 오른쪽 자식 노드들도 나눠준다.
+    }
+
+
+    // 홀수개의 방을 만들어야하는 경우 1 : 2 크기 비율로 split를 계산하기 위함
+    if (n % 2 == 1)
+    {
+        int tmp = (maxLength * leftSize) / (leftSize + 2 * rightSize);
+        split = static_cast<int>(GameEngineRandom::MainRandom.RandomFloat(1.0f - m_rate, 1.0f + m_rate) * (tmp));
+    }
+    else
+    {
+        int tmp = (maxLength * leftSize) / (leftSize + rightSize);
+        split = static_cast<int>(GameEngineRandom::MainRandom.RandomFloat(1.0f - m_rate, 1.0f + m_rate) * (tmp));
+    }
+
+    // split 위치가 사각형을 벗어난 경우
+    if (split < 1 || split > maxLength - 1)
+    {
+        return false;
+    }
+
+    // 나올 수 있는 최대 길이와 최소 길이중에서 랜덤으로 한 값을 선택
+
+
+    {
+        // 방의 여분 비율을 맞추기 위해 split을 조정하는 과정
+        int trycnt = 0;
+        while (true)
+        {
+            trycnt++;
+            if (trycnt > static_cast<int>(maxLength * (1.0f + m_rate)))
+            {
+                break;
+            }
+            GetChildRect(curRect, split, is_height, LeftRect, RightRect);
+            leftSize = GetRoomSize(LeftRect);
+            rightSize = GetRoomSize(RightRect);
+            if (max_room_size * leftnodecnt > leftSize && max_room_size * rightnodecnt > rightSize)
+            {
+                return false;
+            }
+            else if (max_room_size * leftnodecnt > leftSize)
+            {
+                split++;
+            }
+            else if (max_room_size * rightnodecnt > rightSize)
+            {
+                split--;
+            }
+            else
+            {
+                break;
+            }
+        }
+        // 방의 최소 크기를 맞추기 위함이다.
+
+        //왼쪽 노드에 대한 정보다 
+        tree->leftNode = std::make_shared<Node>();
+        tree->leftNode->nodeRect = LeftRect;
+
+        //우측 노드에 대한 정보다 
+        tree->rightNode = std::make_shared<Node>();
+        tree->rightNode->nodeRect = RightRect;
+
+        //그 후 위 두개의 노드를 나눠준 선을 그리는 함수이다.        
+        DrawLine(curRect, split, is_height, n);
+    }
+
+    tree->leftNode->parNode = tree; //자식노드들의 부모노드를 나누기전 노드로 설정
+    tree->rightNode->parNode = tree;
+
+    // 여분비율을 낮춘다
+    float nrate = _rate * spare > 1.0 ? _rate * spare : 1.0f;
+    if (false == is_reverse)
+    {
+        return DivideNode(tree->leftNode, leftnodecnt, _size, nrate, !is_reverse) && DivideNode(tree->rightNode, rightnodecnt, _size, nrate, is_reverse); //왼쪽, 오른쪽 자식 노드들도 나눠준다.
+    }
+    else
+    {
+        return DivideNode(tree->rightNode, rightnodecnt, _size, nrate, !is_reverse) && DivideNode(tree->leftNode, leftnodecnt, _size, nrate, is_reverse); //왼쪽, 오른쪽 자식 노드들도 나눠준다.
+    }
+}
+
+
 
 bool URectRoomMapGenerator::CreateMap(std::vector<std::vector<int>> _map, int _roomcnt, int _min_room_size, int _doorsize)
 {
@@ -316,31 +455,6 @@ bool URectRoomMapGenerator::MakeRoad(const RectInt f_rect, const RectInt s_rect)
         }
         return true;
     }
-    //else if (f_rect.x >= s_rect.x && s_rect.x + s_rect.height > f_rect.x)
-    //{
-    //    int midx = (f_rect.GetMidx() + s_rect.GetMidx()) / 2;
-    //    int miny, maxy;
-
-
-    //    if (s_rect.GetMidy() > f_rect.GetMidy())
-    //    {
-    //        miny = f_rect.GetMidy();
-    //        maxy = s_rect.GetMidy();
-    //    }
-    //    else
-    //    {
-    //        maxy = f_rect.GetMidy();
-    //        miny = s_rect.GetMidy();
-    //    }
-    //    for (int j = miny; j < maxy; ++j)
-    //    {
-    //        if (base_map[midx][j] == 1)
-    //        {
-    //            base_map[midx][j] = 3;
-    //        }
-    //    }
-
-    //}
     // y 축(가로)가 공통인지 확인
     else if ((f_rect.y <= s_rect.y && s_rect.y < f_rect.y + f_rect.width) || (f_rect.y > s_rect.y && s_rect.y + s_rect.width > f_rect.y))
     {
